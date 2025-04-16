7c378e1d914a67b3e777b6cc486fe75802b5b6d2
cglsn <camille.gilson@epfl.ch>
2025-04-16 09:34:33 +0200

    update

---
+++ projet.R
@@ -0,0 +1,12 @@
+# Loading data
+data2025<-(load(file = "/Users/camillegilson/Desktop/Risk and environmental sustainability/Mini-Projet/Sheffield.Tinsley_no2.Rdata"))
+data2025<-Sheffield.Tinsley
+
+# See structure of the object
+head(data2025)
+
+# Creation of a data-frame object
+library(lubridate)
+data.tmp <- data.frame("date"= data2025$date, "value"= data2025$no2,
+                       "year"= year(data2025$date), "month"= month(data2025$date), "day"= day(data2025$date))
+dguzgau
