d79263c07dfea0143737ce32dc5899e4d8a79c38
cglsn <camille.gilson@epfl.ch>
2025-04-16 09:58:11 +0200

    Update projet.R

--- projet.R
+++ projet.R
@@ -8,9 +8,28 @@ head(data2025)
 # Creation of a data-frame object
 library(lubridate)
 data.tmp <- data.frame("date"= data2025$date, "value"= data2025$no2,
-                       "year"= year(data2025$date), "month"= month(data2025$date), "day"= day(data2025$date))
+                     "year"= year(data2025$date), "month"= month(data2025$date), "week" = isoweek(data2025$date), "day"= day(data2025$date))
+
+# compute daily maximum of hourly measurements
 library(dplyr)
 daily_maxima <-data.tmp %>%
   group_by(year, month, day) %>%
   slice_max(order_by = value, n = 1, with_ties = FALSE) %>%
   drop_na() #drop missing observations
+
+# Compute monthly maximum of hourly measurements
+library(dplyr)
+monthly_maxima <-data.tmp %>%
+  group_by(year, month) %>%
+  slice_max(order_by = value, n = 1, with_ties = FALSE) %>%
+  drop_na() #drop missing observations
+
+# Compute weekly maximum of hourly measurements
+library(dplyr)
+monthly_maxima <-data.tmp %>%
+  group_by(year, month) %>%
+  slice_max(order_by = value, n = 1, with_ties = FALSE) %>%
+  drop_na() #drop missing observations
+
+
+
