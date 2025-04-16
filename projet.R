f9035ea0578b30c29b30ddd5584e4996331b30e2
cglsn <camille.gilson@epfl.ch>
2025-04-16 09:36:58 +0200

    update

--- projet.R
+++ projet.R
@@ -9,4 +9,8 @@ head(data2025)
 library(lubridate)
 data.tmp <- data.frame("date"= data2025$date, "value"= data2025$no2,
                        "year"= year(data2025$date), "month"= month(data2025$date), "day"= day(data2025$date))
-dguzgau
+library(dplyr)
+daily_maxima <-data.tmp %>%
+  group_by(year, month, day) %>%
+  slice_max(order_by = value, n = 1, with_ties = FALSE) %>%
+  drop_na() #drop missing observations
